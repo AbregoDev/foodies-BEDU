e0abc394f4ebc5662b2094a42dedbc1d5bf3c06f
ernestov <er.velasco94@gmail.com>
2021-08-12 16:02:37 -0500

    Se agrego seccion de favoritos en index

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,18 @@
 import css from "../styles.css";
 import imgGatito from "../assets/IMG_1321.jpg";
+import imgGatito2 from "../assets/IMG_1321.jpg";
+import imgGatito3 from "../assets/IMG_1321.jpg";
+import imgGatito4 from "../assets/IMG_1321.jpg";
+import imgGatito5 from "../assets/IMG_1321.jpg";
+import imgGatito6 from "../assets/IMG_1321.jpg";
 
-document.getElementById('gatito').src = imgGatito;
+import imgFlag from "../assets/Flag.png"
+
+document.querySelector('#gatito').src = imgGatito;
+document.querySelector('#gatito2').src= imgGatito2;
+document.querySelector('#gatito3').src= imgGatito3;
+document.querySelector('#gatito4').src = imgGatito4;
+document.querySelector('#gatito5').src= imgGatito5;
+document.querySelector('#gatito6').src= imgGatito6;
+
+document.querySelector('#flag').src= imgFlag; 
