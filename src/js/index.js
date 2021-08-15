a66e936c61fb4db592004ad4d3c7456c78920074
Maria Fernanda <mafer13cp@gmail.com>
2021-08-15 00:45:12 -0600

    Cambios al index: imagen principal y cards de favoritos

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,43 @@
+//IMPORTACIONES
 import css from "../styles.css";
-import imgGatito from "../assets/IMG_1321.jpg";
-import imgGatito2 from "../assets/IMG_1321.jpg";
-import imgGatito3 from "../assets/IMG_1321.jpg";
-import imgGatito4 from "../assets/IMG_1321.jpg";
-import imgGatito5 from "../assets/IMG_1321.jpg";
-import imgGatito6 from "../assets/IMG_1321.jpg";
+//Header principal
+import imgMain from "../assets/Main.png";
 
+//Our favs
+import imgFav1 from "../assets/IMG_1321.jpg";
+import imgFav2 from "../assets/IMG_1321.jpg";
+import imgFav3 from "../assets/IMG_1321.jpg";
+import imgFav4 from "../assets/IMG_1321.jpg";
+import imgFav5 from "../assets/IMG_1321.jpg";
+import imgFav6 from "../assets/IMG_1321.jpg";
+
+
+//Países
 import imgFlag from "../assets/Flag.png"
 
+//Footer
 import imgBreakfast from "../assets/Breakfast.png"
-
+ 
 import imgIconoMX from "../assets/iconoMX.png";
 
-document.querySelector('#gatito').src = imgGatito;
-document.querySelector('#gatito2').src= imgGatito2;
-document.querySelector('#gatito3').src= imgGatito3;
-document.querySelector('#gatito4').src = imgGatito4;
-document.querySelector('#gatito5').src= imgGatito5;
-document.querySelector('#gatito6').src= imgGatito6;
 
+//ASIGNAR RECURSOS IMPORTADOS
+
+//Header principal 
+document.querySelector("#background-main-img").src = imgMain;
+
+//Our favs
+document.querySelector('#favorites1').src = imgFav1;
+document.querySelector('#favorites2').src= imgFav2;
+document.querySelector('#favorites3').src= imgFav3;
+document.querySelector('#favorites4').src = imgFav4;
+document.querySelector('#favorites5').src= imgFav5;
+document.querySelector('#favorites6').src= imgFav6;
+
+//Países
 document.querySelector('#flag').src= imgFlag; 
 
+//Footer
 document.querySelector('#breakfast').src= imgBreakfast;
 
 document.querySelector('#iconoMX').src=imgIconoMX;
