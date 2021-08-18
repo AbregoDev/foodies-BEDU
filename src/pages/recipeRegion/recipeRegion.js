3b378f7c67daa6a8681c7b7d891df0fb7788774f
unknown <pepe96_good@hotmail.com>
2021-08-18 03:35:05 -0500

    Tarjetas dinámicas, desde index.html se envía variable con valor de la region

--- src/pages/recipeRegion/recipeRegion.js
+++ src/pages/recipeRegion/recipeRegion.js
@@ -1,20 +1,94 @@
 import recipeRegionCSS from "./recipeRegion.css"
-
-import imgReceta1 from "../../assets/enchilada.jpg";
-import imgReceta2 from "../../assets/enchilada.jpg";
-import imgReceta3 from "../../assets/enchilada.jpg";
-import imgReceta4 from "../../assets/enchilada.jpg";
-import imgReceta5 from "../../assets/enchilada.jpg";
-import imgReceta6 from "../../assets/enchilada.jpg";
-
 import imgIconoMX from "../../assets/iconoMX.png";
 
-
-document.querySelector('#receta1').src= imgReceta1;
-document.querySelector('#receta2').src= imgReceta2;
-document.querySelector('#receta3').src= imgReceta3;
-document.querySelector('#receta4').src= imgReceta4;
-document.querySelector('#receta5').src= imgReceta5;
-document.querySelector('#receta6').src= imgReceta6;
-
 document.querySelector('#iconoMX').src= imgIconoMX;
+
+// variable obtenida en index.html
+var region = localStorage.getItem("region");
+
+const titulo = document.getElementsByTagName('h4')[0];
+titulo.innerHTML = region + " Food";
+
+const app = document.querySelector('#app');
+
+getMealByRegion()
+  .then(function (data) {
+
+    data.meals.forEach(function (data) {
+        var baseUrl = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+        baseUrl += data.idMeal;
+        getMealbyId (baseUrl)
+        .then(function (data) {
+            
+            let article = document.createElement('article');
+            article.className = 'col s4';
+
+            let div1 = document.createElement('div');
+            div1.className = "card fav";
+
+            let div2 = document.createElement('div');
+            div2.className = "card-image";
+            
+            let img = document.createElement('img');
+            img.className = "responsive-img";
+            img.src = data.meals[0].strMealThumb; // Link Imagen receta
+
+            let a = document.createElement('a');
+            a.className = "btn-floating halfway-fab waves-effect waves-light red";
+
+            let i = document.createElement('i');
+            i.className = "material-icons";
+            i.textContent = "arrow_forward";
+
+            let div3 = document.createElement('div');
+            div3.className = "card-content";
+
+            let span = document.createElement('span');
+            span.className = "card-title";
+            span.textContent = data.meals[0].strMeal; // Nombre receta
+
+            let p = document.createElement('p');
+            p.textContent = getTags(data.meals[0].strTags); // Tags receta
+
+            a.appendChild(i);
+            div2.appendChild(img);
+            div2.appendChild(a);
+            div3.appendChild(span);
+            div3.appendChild(p);
+            div1.appendChild(div2);
+            div1.appendChild(div3);
+            article.appendChild(div1);
+
+            app.appendChild(article);
+
+        });
+    })
+  })
+
+
+function getMealByRegion() {
+    return fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a='+region)
+    .then(function (response) {
+      return response.json();
+    })
+}
+
+function getMealbyId (urlId){
+    return fetch(urlId)
+        .then(function(response) {
+            return response.json();
+        })
+}
+
+function getTags (stringTags) {
+    var tags = "";
+    if (stringTags == null) {return tags = "No tags."}
+    var arrayTags = stringTags.split(',');
+    for (var i = 0; i < arrayTags.length; i++)
+    {
+        if (i == arrayTags.length-1) {tags+= "#"+ arrayTags[i];}
+        else {tags+= "#" + arrayTags[i] + ", ";}
+        
+    }
+    return tags;
+}
