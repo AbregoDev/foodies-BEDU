f648e4b9ebb10e5d783008c1cb43b4c104f80fe7
Maria Fernanda <mafer13cp@gmail.com>
2021-08-18 18:37:07 -0600

    Página de búscar conectada con la API, muestra los resultados

--- src/pages/searchResult/searchResult.js
+++ src/pages/searchResult/searchResult.js
@@ -3,38 +3,167 @@ import imgIconoMX from "../../assets/iconoMX.png";
 
 document.querySelector('#iconoMX').src= imgIconoMX;
 
+// variable obtenida en index.html
+var search = localStorage.getItem("search");
+search = "chicken";
 
-var search = "chicken";
+//Cambiar nombres
+const titulo = document.getElementsByTagName('h4');
+titulo[0].innerHTML = search + " recipes";
+titulo[2].innerHTML = "Made with " + search;
 
-var favs = document.getElementsByClassName("card fav");
-console.log(favs);
-var ourFav = [52854, 52858, 52895, 53014, 53013, 52765];
-// favoritos: pancakes, cheesecake, english breakfast, pizza, big mac, enchilada
+const byName = document.querySelector('#byName');
+const madeWith = document.querySelector('#madeWith');
 
+//Recetas que coincidan con la búsqueda
+getMealByName()
+  .then(function (data) {
+    console.log(data);
+    if (data.meals == null)
+    {
+        noMeals(byName)
+    }
+    else 
+    {
+        data.meals.forEach(function (data) {
+            var baseUrl = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+            baseUrl += data.idMeal;
+            getMealbyId (baseUrl)
+            .then(function (data) {
+                
+                console.log(data);
+                
+                let article = document.createElement('article');
+                article.className = 'col s4';
 
-var indexF = 0;
-while (indexF < 6)
-{
-    assignById(ourFav[indexF], indexF);
-    indexF++;
+                let div1 = document.createElement('div');
+                div1.className = "card fav";
+
+                let div2 = document.createElement('div');
+                div2.className = "card-image";
+                
+                let img = document.createElement('img');
+                img.className = "responsive-img";
+                img.src = data.meals[0].strMealThumb; // Link Imagen receta
+
+                let a = document.createElement('a');
+                a.className = "btn-floating halfway-fab waves-effect waves-light red";
+                // Link to recipe details
+                const recipeId = data.meals[0].idMeal
+                const recipeLink = `recipeDetail.html?id=${recipeId}`;
+                a.href = recipeLink;
+
+                let i = document.createElement('i');
+                i.className = "material-icons";
+                i.textContent = "arrow_forward";
+
+                let div3 = document.createElement('div');
+                div3.className = "card-content";
+
+                let span = document.createElement('span');
+                span.className = "card-title";
+                span.textContent = data.meals[0].strMeal; // Nombre receta
+
+                let p = document.createElement('p');
+                p.textContent = getTags(data.meals[0].strTags); // Tags receta
+
+                a.appendChild(i);
+                div2.appendChild(img);
+                div2.appendChild(a);
+                div3.appendChild(span);
+                div3.appendChild(p);
+                div1.appendChild(div2);
+                div1.appendChild(div3);
+                article.appendChild(div1);
+
+                byName.appendChild(article);
+
+            }); 
+        })
+    }    
+  })
+
+  //Recetas que contengan el ingrediente
+  getMealByIngredient()
+  .then(function (data) {
+    console.log(data);
+    if (data.meals == null)
+    {
+        noMeals(madeWith);
+    }
+    else 
+    {
+        data.meals.forEach(function (data) {
+            var baseUrl = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+            baseUrl += data.idMeal;
+            getMealbyId (baseUrl)
+            .then(function (data) {
+                
+                console.log(data);
+                
+                let article = document.createElement('article');
+                article.className = 'col s4';
+
+                let div1 = document.createElement('div');
+                div1.className = "card fav";
+
+                let div2 = document.createElement('div');
+                div2.className = "card-image";
+                
+                let img = document.createElement('img');
+                img.className = "responsive-img";
+                img.src = data.meals[0].strMealThumb; // Link Imagen receta
+
+                let a = document.createElement('a');
+                a.className = "btn-floating halfway-fab waves-effect waves-light red";
+                // Link to recipe details
+                const recipeId = data.meals[0].idMeal
+                const recipeLink = `recipeDetail.html?id=${recipeId}`;
+                a.href = recipeLink;
+
+                let i = document.createElement('i');
+                i.className = "material-icons";
+                i.textContent = "arrow_forward";
+
+                let div3 = document.createElement('div');
+                div3.className = "card-content";
+
+                let span = document.createElement('span');
+                span.className = "card-title";
+                span.textContent = data.meals[0].strMeal; // Nombre receta
+
+                let p = document.createElement('p');
+                p.textContent = getTags(data.meals[0].strTags); // Tags receta
+
+                a.appendChild(i);
+                div2.appendChild(img);
+                div2.appendChild(a);
+                div3.appendChild(span);
+                div3.appendChild(p);
+                div1.appendChild(div2);
+                div1.appendChild(div3);
+                article.appendChild(div1);
+
+                madeWith.appendChild(article);
+
+            }); 
+        }) 
+    }
+  })
+
+function getMealByName() {
+    return fetch('https://www.themealdb.com/api/json/v1/1/search.php?s='+search)
+    .then(function (response) {
+      return response.json();
+    })
 }
 
-function assignById (id, index) {
-    var baseUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
-    baseUrl += search;
-    getMealbyId (baseUrl)
-    .then(function (data) {
-        console.log(data);
-        //Imagen
-        favs[index].children[0].children[0].src = data.meals[index].strMealThumb;
-        //Título = Nombre platillo
-        favs[index].children[1].children[0].innerHTML = data.meals[index].strMeal;
-        //Tags
-        favs[index].children[1].children[1].innerHTML = getTags(data.meals[index].strTags);
-        console.log(getTags(data.meals[0].strTags));
-    });
+function getMealByIngredient() {
+    return fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i='+search)
+    .then(function (response) {
+      return response.json();
+    })
 }
-
 
 function getMealbyId (urlId){
     return fetch(urlId)
@@ -54,4 +183,22 @@ function getTags (stringTags) {
         
     }
     return tags;
+}
+
+function noMeals (parent){
+    console.log("sad");
+    let article = document.createElement('article');
+    article.className = "col s10 center";
+    let h4 = document.createElement('h4');
+    h4.className = "title-no-found";
+    h4.textContent = "No results found for " + search + ".";
+    let i = document.createElement('i');
+    i.id = "sad-face";
+    i.className = "material-icons";
+    i.textContent = "sentiment_dissatisfied";
+    i.style.fontSize = 60;
+
+    article.appendChild(h4);
+    article.appendChild(i);
+    parent.appendChild(article);
 }
